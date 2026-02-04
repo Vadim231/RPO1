c0b7af8d8c8f3cc9ad7eaca8f3d981b19ff18c42
1mposs1blyt <alexandr.bryaginya@gmail.com>
2026-02-04 16:03:28 +0700

    ElectronJS + Vite + React + TS

---
+++ front/src/main.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './styles/App.css'
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
+
+// Use contextBridge
+window.ipcRenderer.on('main-process-message', (_event, message) => {
+  console.log(message)
+})
