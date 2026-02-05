c7651354cc63631bd03a9b6c47eea55804a72d36
1mposs1blyt <alexandr.bryaginya@gmail.com>
2026-02-06 04:11:46 +0700

    avatar component

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,7 +1,7 @@
 export default function App() {
   return (
     <>
-      <h1 className="text-blue-800 text-5xl text-center mb-3">ElectronJS + Vite + React + TS</h1>
+      <h1 className="text-blue-800 text-5xl text-center mb-3 select-none">ElectronJS + Vite + React + TS</h1>
     </>
   )
 }
