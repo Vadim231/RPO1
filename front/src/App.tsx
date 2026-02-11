b51ed5458ecc5348050230debe61a0cd53bbaa01
1mposs1blyt <alexandr.bryaginya@gmail.com>
2026-02-12 04:19:39 +0700

    фикс

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -154,125 +154,131 @@ export default function App() {
     <div className="h-screen w-screen flex flex-col overflow-hidden bg-base-300">
       {window.electronAPI ? <TopMenu /> : ""}
       <main className={`flex-1 overflow-y-hidden ${window.electronAPI ? "mt-6" : ""}`}>
-        <div className="h-14 w-full bg-base-300">{/* Тут возможно будут всякие кнопки как в тг */}</div>
-        <div className="w-auto flex h-max max-h-svh overflow-y-hidden">
-          <div className={`bg-primary/15 w-1/3 ${window.electronAPI ? "pb-20" : "pb-14"} overflow-y-scroll [&::-webkit-scrollbar]:hidden [-ms-overflow-style:none] [scrollbar-width:none]`}>
-            <ChatList chats={chats} />
+        <div className="w-auto flex h-max max-h-svh flex-col">
+          <div className="h-36 w-full bg-base-300 flex flex-row justify-between">
+            <div className="w-1/3 bg-base overflow-y-hidden">content</div>
+            <div className="w-full bg-base-200 pl-4 pr-4">content</div>
+            {/* Тут возможно будут всякие кнопки как в тг */}
           </div>
-          <div className={`bg-primary/25 w-full ${window.electronAPI ? "pb-20" : "pb-14"} pl-4 pr-4 overflow-y-scroll [&::-webkit-scrollbar]:hidden [-ms-overflow-style:none] [scrollbar-width:none]`}>
-            <Chat modifier={"sender"} message={
-              <Message messageText="Hello, world!" userName={"My username"} userAvatar={
-                <Avatar
-                  shape="circle"
-                  color="primary"
-                  status="online"
-                  iconUrl="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Typescript_logo_2020.svg/960px-Typescript_logo_2020.svg.png" isChat={true} />
-              } timeStamp={"12:25"} messageStatus="read" />
-            } />
-            <Chat modifier={"sender"} message={
-              <Message messageText="Hello, world!" userName={"My username"} userAvatar={
-                <Avatar color="primary"
-                  status="online"
-                  iconUrl="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Typescript_logo_2020.svg/960px-Typescript_logo_2020.svg.png" isChat={true} />
-              } timeStamp={"12:25"} messageStatus="recieved" />
-            } />
-            <Chat modifier="reciever" message={
-              <Message messageText="Hello, world!" userName={"My username"} userAvatar={
-                <Avatar color="primary"
-                  status="online"
-                  iconUrl="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Typescript_logo_2020.svg/960px-Typescript_logo_2020.svg.png" isChat={true} />
-              } timeStamp={"12:25"} messageStatus="sent" />
-            } />
-            <Chat modifier={"sender"} message={
-              <Message messageText="Hello, world!" userName={"My username"} userAvatar={
-                <Avatar color="primary"
-                  status="online"
-                  iconUrl="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Typescript_logo_2020.svg/960px-Typescript_logo_2020.svg.png" isChat={true} />
-              } timeStamp={"12:25"} messageStatus="read" />
-            } />
-            <Chat modifier="reciever" message={
-              <Message messageText="Hello, world!" userName={"My username"} userAvatar={
-                <Avatar color="primary"
-                  status="online"
-                  iconUrl="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Typescript_logo_2020.svg/960px-Typescript_logo_2020.svg.png" isChat={true} />
-              } timeStamp={"12:25"} messageStatus="recieved" />
-            } />
-            <Chat modifier="reciever" message={
-              <Message messageText="Hello, world!" userName={"My username"} userAvatar={
-                <Avatar color="primary"
-                  status="online"
-                  iconUrl="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Typescript_logo_2020.svg/960px-Typescript_logo_2020.svg.png" isChat={true} />
-              } timeStamp={"12:25"} messageStatus="recieved" />
-            } />
-            <Chat modifier={"sender"} message={
-              <Message messageText="Hello, world!" userName={"My username"} userAvatar={
-                <Avatar color="primary"
-                  status="online"
-                  iconUrl="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Typescript_logo_2020.svg/960px-Typescript_logo_2020.svg.png" isChat={true} />
-              } timeStamp={"12:25"} messageStatus="read" />
-            } />
-            <Chat modifier={"sender"} message={
-              <Message messageText="Hello, world!" userName={"My username"} userAvatar={
-                <Avatar color="primary"
-                  status="online"
-                  iconUrl="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Typescript_logo_2020.svg/960px-Typescript_logo_2020.svg.png" isChat={true} />
-              } timeStamp={"12:25"} messageStatus="recieved" />
-            } />
-            <Chat modifier="reciever" message={
-              <Message messageText="Hello, world!" userName={"My username"} userAvatar={
-                <Avatar color="primary"
-                  status="online"
-                  iconUrl="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Typescript_logo_2020.svg/960px-Typescript_logo_2020.svg.png" isChat={true} />
-              } timeStamp={"12:25"} messageStatus="sent" />
-            } />
-            <Chat modifier="sender" message={
-              <Message
-                userName={""}
-                userAvatar={undefined}
-                timeStamp={"12:34"}
-                messageStatus={"read"}
-                fileAttached={myFile}
-              />
-            } />
-            <Chat key={"uniqKey"} modifier="reciever" message={
-              <Message
-                key={"uniqKey3"}
-                userName="penis"
-                userAvatar=""
-                timeStamp="1254"
-                messageStatus="recieved"
-                imageAttached={myImage}
-              />
-            } />
-            <Chat key={"uniqKey2"} modifier="sender" message={
-              <Message
-                messageId={"SuperUniqId"}
-                key={"uniqKey4"}
-                userName=""
-                userAvatar=""
-                timeStamp=""
-                messageStatus="recieved"
-                galleryAttached={myGallery}
-              />
-            } />
-            <Chat modifier="sender" message={
-              <Message
-                userName=""
-                userAvatar=""
-                timeStamp=""
-                messageStatus="recieved"
-                sticker="https://cdn-icons-png.flaticon.com/256/4288/4288932.png"
-              />
-            } />
-            <Chat modifier="reciever" message={
-              <Message
-                userName=""
-                userAvatar=""
-                timeStamp=""
-                messageStatus="recieved"
-                sticker="https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExMWRoeHJmeDNoOWU1MXdwZmh6enp0dXI4bTI2aWY4ZWM2cTlwbHZkOSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/VwOVl2sdbJf9dxr7O6/giphy.gif"
-              />
-            } />
+          <div className="flex flex-row overflow-y-hidden">
+            <div className={`bg-primary/15 w-1/3 ${window.electronAPI ? "pb-6" : "pb-14"} overflow-y-scroll [&::-webkit-scrollbar]:hidden [-ms-overflow-style:none] [scrollbar-width:none]`}>
+              <ChatList chats={chats} />
+            </div>
+            <div className={`bg-primary/25 w-full ${window.electronAPI ? "pb-6" : "pb-14"} pl-4 pr-4 overflow-y-scroll [&::-webkit-scrollbar]:hidden [-ms-overflow-style:none] [scrollbar-width:none]`}>
+              <Chat modifier={"sender"} message={
+                <Message messageText="Hello, world!" userName={"My username"} userAvatar={
+                  <Avatar
+                    shape="circle"
+                    color="primary"
+                    status="online"
+                    iconUrl="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Typescript_logo_2020.svg/960px-Typescript_logo_2020.svg.png" isChat={true} />
+                } timeStamp={"12:25"} messageStatus="read" />
+              } />
+              <Chat modifier={"sender"} message={
+                <Message messageText="Hello, world!" userName={"My username"} userAvatar={
+                  <Avatar color="primary"
+                    status="online"
+                    iconUrl="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Typescript_logo_2020.svg/960px-Typescript_logo_2020.svg.png" isChat={true} />
+                } timeStamp={"12:25"} messageStatus="recieved" />
+              } />
+              <Chat modifier="reciever" message={
+                <Message messageText="Hello, world!" userName={"My username"} userAvatar={
+                  <Avatar color="primary"
+                    status="online"
+                    iconUrl="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Typescript_logo_2020.svg/960px-Typescript_logo_2020.svg.png" isChat={true} />
+                } timeStamp={"12:25"} messageStatus="sent" />
+              } />
+              <Chat modifier={"sender"} message={
+                <Message messageText="Hello, world!" userName={"My username"} userAvatar={
+                  <Avatar color="primary"
+                    status="online"
+                    iconUrl="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Typescript_logo_2020.svg/960px-Typescript_logo_2020.svg.png" isChat={true} />
+                } timeStamp={"12:25"} messageStatus="read" />
+              } />
+              <Chat modifier="reciever" message={
+                <Message messageText="Hello, world!" userName={"My username"} userAvatar={
+                  <Avatar color="primary"
+                    status="online"
+                    iconUrl="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Typescript_logo_2020.svg/960px-Typescript_logo_2020.svg.png" isChat={true} />
+                } timeStamp={"12:25"} messageStatus="recieved" />
+              } />
+              <Chat modifier="reciever" message={
+                <Message messageText="Hello, world!" userName={"My username"} userAvatar={
+                  <Avatar color="primary"
+                    status="online"
+                    iconUrl="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Typescript_logo_2020.svg/960px-Typescript_logo_2020.svg.png" isChat={true} />
+                } timeStamp={"12:25"} messageStatus="recieved" />
+              } />
+              <Chat modifier={"sender"} message={
+                <Message messageText="Hello, world!" userName={"My username"} userAvatar={
+                  <Avatar color="primary"
+                    status="online"
+                    iconUrl="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Typescript_logo_2020.svg/960px-Typescript_logo_2020.svg.png" isChat={true} />
+                } timeStamp={"12:25"} messageStatus="read" />
+              } />
+              <Chat modifier={"sender"} message={
+                <Message messageText="Hello, world!" userName={"My username"} userAvatar={
+                  <Avatar color="primary"
+                    status="online"
+                    iconUrl="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Typescript_logo_2020.svg/960px-Typescript_logo_2020.svg.png" isChat={true} />
+                } timeStamp={"12:25"} messageStatus="recieved" />
+              } />
+              <Chat modifier="reciever" message={
+                <Message messageText="Hello, world!" userName={"My username"} userAvatar={
+                  <Avatar color="primary"
+                    status="online"
+                    iconUrl="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Typescript_logo_2020.svg/960px-Typescript_logo_2020.svg.png" isChat={true} />
+                } timeStamp={"12:25"} messageStatus="sent" />
+              } />
+              <Chat modifier="sender" message={
+                <Message
+                  userName={""}
+                  userAvatar={undefined}
+                  timeStamp={"12:34"}
+                  messageStatus={"read"}
+                  fileAttached={myFile}
+                />
+              } />
+              <Chat key={"uniqKey"} modifier="reciever" message={
+                <Message
+                  key={"uniqKey3"}
+                  userName="penis"
+                  userAvatar=""
+                  timeStamp="1254"
+                  messageStatus="recieved"
+                  imageAttached={myImage}
+                />
+              } />
+              <Chat key={"uniqKey2"} modifier="sender" message={
+                <Message
+                  messageId={"SuperUniqId"}
+                  key={"uniqKey4"}
+                  userName=""
+                  userAvatar=""
+                  timeStamp=""
+                  messageStatus="recieved"
+                  galleryAttached={myGallery}
+                />
+              } />
+              <Chat modifier="sender" message={
+                <Message
+                  userName=""
+                  userAvatar=""
+                  timeStamp=""
+                  messageStatus="recieved"
+                  sticker="https://cdn-icons-png.flaticon.com/256/4288/4288932.png"
+                />
+              } />
+              <Chat modifier="reciever" message={
+                <Message
+                  userName=""
+                  userAvatar=""
+                  timeStamp=""
+                  messageStatus="recieved"
+                  sticker="https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExMWRoeHJmeDNoOWU1MXdwZmh6enp0dXI4bTI2aWY4ZWM2cTlwbHZkOSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/VwOVl2sdbJf9dxr7O6/giphy.gif"
+                />
+              } />
+            </div>
           </div>
         </div>
       </main>
